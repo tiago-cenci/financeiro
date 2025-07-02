c60b546b354a1ef3c22ef372c49ac89c7d4f752b
Tiago <tiagocenci@alunos.utfpr.edu.br>
2025-07-02 20:20:55 -0300

    ip

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import Config
 from models import db
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,5 +12,7 @@ db.init_app(app)
 def index():
     return 'API Controle Financeiro OK ✅'
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
