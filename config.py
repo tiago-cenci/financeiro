0f2ded9d3056a3a1e3d4f6325346945cfbdbf5d6
Tiago <tiagocenci@alunos.utfpr.edu.br>
2025-07-02 20:36:31 -0300

    fix ipv6

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:01Thiago!@db.lmautjxltjmiywsivrhf.supabase.co:5432/postgres")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:01Thiago!@db.lmautjxltjmiywsivrhf.supabase.co:5432/postgres?sslmode=require&options=--inet4")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
